302c6e947c7ff644ac3281c9bd4963e7624b9ee5
adblackx <47539000+adblackx@users.noreply.github.com>
2019-04-18 21:28:10 +0200

    Update coor.cpp
    Commentaires sur surprimeEC

--- coor.cpp
+++ coor.cpp
@@ -7,20 +7,19 @@ using namespace std;
     cout << "Test failed in file " << __FILE__ << " line " << __LINE__         \
          << ": " #test << endl
 
-
-
-
-
-
-
+const int MAXENSCOORD=15; 
 
 
 struct coord {
     int ligne;
     int colonne;
-  };
-  
-  
+  }; 
+ 
+ struct EnsCoord{
+      coord tab[MAXENSCOORD] ;
+      int nbElts;
+        
+ } ;
   
  coord creerCoord(int ligne,int colonne){
       coord c;
@@ -50,20 +49,11 @@ bool egalCoord(coord c1,coord c2){
      return c1.ligne==c2.ligne and c1.colonne==c2.colonne;
    } 
  
- const int MAXENSCOORD=15; 
- 
- 
- struct EnsCoord{
-      coord tab[MAXENSCOORD] ;
-      int nbElts;
-        
- } ;
- 
  
  void afficheEC(EnsCoord ec){
      for(int i=0;i<ec.nbElts;i++) {
         afficheCoord(ec.tab[i]);
-  }
+     }
  }
  
  
@@ -101,7 +91,13 @@ bool egalCoord(coord c1,coord c2){
  
  
  //** c'est faux
- void surprimeEC(EnsCoord &ec,coord c){
+ /* Tu cherche la coordonnée dans le tableau
+ * Une fois trouvée tu enregistres l'indexe 
+ * Puis tu copies les elements dans l'indexe n+1 dans n
+ * tu soustraies le nombre d'elements dans le tableau
+ * attention a ne pas déborder dans le tableau....
+ */ 
+ void surprimeEC(EnsCoord &ec,coord c){ 
      ec.tab[ec.nbElts]=c;
      ec.nbElts=ec.nbElts-1;
      }
@@ -118,33 +114,4 @@ afficheCoord(c1);
 cout << endl;
 return 0;
            
-}           
-           
-           
-           
-           
-         
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-
-      
-    
+} 
