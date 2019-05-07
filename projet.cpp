538ed3aa6ec67e0028f1d89d6a7855ea1485b7b4
ness646 <49685457+ness646@users.noreply.github.com>
2019-05-07 12:45:48 +0200

    Update projet.cpp

--- projet.cpp
+++ projet.cpp
@@ -5,6 +5,26 @@
 
 using namespace std;
 
+
+
+EnsCoord toutEspece(grille g, Espece e) {
+  EnsCoord ec;
+  coord c;
+  for(int i=0;i<ec.nbElts;i++){
+     for(int j=0;j<ec.nbElts;j++){
+          c=creerCoord(j,i);
+      if (especeAnimal(getAnimal(g,c))==e) {
+        ajouteEC(ec,c);
+      }
+    }
+  }
+  return ec;
+}
+
+
+
+
+
 int main(){
 	cout<<hasard()<<endl;
 	grille g;
