fe970a4f334e76874ebd3781c24eb2aba23fd547
ness646 <49685457+ness646@users.noreply.github.com>
2019-05-09 14:56:38 +0200

    Update projet.cpp

--- projet.cpp
+++ projet.cpp
@@ -6,22 +6,6 @@
 using namespace std;
 
 
-
-EnsCoord toutEspece(grille g, Espece e) {
-  EnsCoord ec;
-  coord c;
-  for(int i=0;i<ec.nbElts;i++){
-     for(int j=0;j<ec.nbElts;j++){
-          c=creerCoord(j,i);
-      if (especeAnimal(getAnimal(g,c))==e) {
-        ajouteEC(ec,c);
-      }
-    }
-  }
-  return ec;
-}
-
-
 EnsCoord toutEspece(grille g, Espece e) {
   EnsCoord ec=CreerEC();
   coord c;
@@ -37,30 +21,32 @@ EnsCoord toutEspece(grille g, Espece e) {
 }
 
 
-//void deplaceanimal(
 
 
 
-void deplacetousleslapins(grille g, grille &ng){
-  EnsCoord ec=CreerEC();
-  ec=toutEspece(g, Lapin);
-  coord c;
-  Animal a;
-  while(cardEC(ec)!=0){
-    c= randomEC(ec);
-    a=getAnimal(g,c);
-    deplaceAnimal(g, ng, a);
-    int nbVide = cardEC(voisinvide(g,c));
-    if(seReproduitAnimal(a,nbVide)){
-      setAnimal(ng, creerAnimal(Lapin,c));
+
+bool attaqueRenard(grille g, Animal &a){
+  if( especeAnimal(a) == Renard ){
+    EnsCoord ec=CreerEC();
+    ec=toutEspece(g,Renard);
+    if(cardEC(ec) == 0 ){
+      return false;
+    } else {
+      coord c = randomEC(ec);
+      changeCoordAnimal(a,c);
+      mangeRenard(a);
+      return true;
     }
-    supprimeEC(ec, c);
+  } else {
+    cout << "N est pas un renard ! Ne peut pas attaquer !" << endl;
+        return false;
   }
 }
 
-    
 
-   
+
+
+
 
 
 
@@ -87,35 +73,101 @@ void deplaceAnimal(grille g, grille &ng, Animal &a){
 }
 
 
-
-
-bool attaqueRenard(grille g, Animal &a){
-  if( especeAnimal(a) == Renard ){
-    EnsCoord ec = (g, coordAnimal(a));
-    if(cardEC(ec) == 0 ){
-      return false;
-    } else {
-      coord c = randomEC(ec);
-      changeCoordAnimal(a, c);
-      mangeRenard(a);
-      return true;
+void deplacetousleslapins(grille g, grille &ng){
+  EnsCoord ec=CreerEC();
+  ec=toutEspece(g, Lapin);
+  coord c;
+  Animal a;
+  while(cardEC(ec)!=0){
+    c= randomEC(ec);
+    a=getAnimal(g,c);
+    deplaceAnimal(g, ng, a);
+    int nbVide = cardEC(voisinvide(g,c));
+    if(seReproduitAnimal(a,nbVide)){
+      setAnimal(ng, creerAnimal(Lapin,c));
     }
-  } else {
-    cout << "N est pas un renard ! Ne peut pas attaquer !" << endl;
-        return false;
+    supprimeEC(ec, c);
   }
 }
 
+   /* void deplacetousleslapins(grille g1,grille &g2){
+    EnsCoord E= toutEspece(g1,Lapin);
+    EnsCoord V;
+    for(int i = 0;i< cardEC(E);i++){
+        Animal a = getAnimal(g1, E.tab[i]);
+        V= voisinvide(g2,coordAnimal(a));
+        if(seReproduitAnimal(a,V.nbElts)){
+            setAnimal(g2,a);
+        }
+        if(V.nbElts > 0){
+            coord c = randomEC(V);
+            changeCoordAnimal(a,c);
+            setAnimal(g2,a);
+        }else{
+            cout<<"test"<<endl;
+        }
+    }
+}
+*/
 
+void deplacetouslesrenard(grille g,grille &ng){
+    EnsCoord e = toutEspece(g,Renard);
+    EnsCoord v;
+    for(int i = 0;i< cardEC(e);i++){
+        Animal a = getAnimal(g, e.tab[i]);
+        v = voisinvide(ng,coordAnimal(a));
+        if(seReproduitAnimal(a,v.nbElts)){
+            setAnimal(ng,a);
+        }
+        if(attaqueRenard(g,a)){
+        setAnimal(ng,a);
+        }
+        else if(v.nbElts > 0){
+            coord c = randomEC(v);
+            changeCoordAnimal(a,c);
+            setAnimal(ng,a);
+        }
+        faimRenard(a);
+        if(mortAnimal(a)){
+            creerAnimal(Vide,coordAnimal(a));
+        }
+    }
+}
 
 
 
 int main(){
-	cout<<hasard()<<endl;
-	grille g;
-	grilleVide(g);
-	afficheGrille(g);
-	initialiseGrille(g);
-	afficheGrille(g);
-  return 0;
+	 srand(time(NULL)); //Pour que la fonction aleatoire ne soit pas tt le temps la meme
+
+ grille g1, g2;
+
+ initialiseGrille(g2);
+ afficheGrille(g2);
+ int lapins, renards;
+
+ compteEspeces(g2, lapins, renards);
+	
+ while ( (lapins != 0) && (renards != 0) ){
+
+    grilleVide(g1);
+	copieGrille(g2,g1);
+	
+	
+	
+ 	 deplacetousleslapins(g2,g1);
+
+ 	 deplacetouslesrenard(g2,g1);
+
+     
+ 	 afficheGrille(g1);
+     //afficheGrille(g2);
+ }
+
+ 
+
+ 
+
+ return 0;
+
 }
+
